02717f4feafcf6b0db62fb1f1336d43c4cbc8b1c
segir187 <106813125+segir187@users.noreply.github.com>
2022-06-03 13:12:49 +0200

    Create index_school.php

---
+++ index_school.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+	<meta charset="utf-8" />
+	<title>Baza lekarzy</title>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+</head>
+<body>
+	<div id="main">
+	<div id="top_banner">
+		<h1>Baza lekarzy</h1>
+	</div>
+	<div id="menu">
+		<a href="index_school.php">Strona główna</a>
+		<a href="display_school.php">Znajdź lekarzy</a>
+		<a href="add_school.php">Dodaj lekarza</a>
+		<a href="edit_school.php">Edytuj dane lekarza</a>
+		<a href="delete_school.php">Usuń lekarza</a>
+	</div>
+	<div id="container">
+		<br>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+	</div>
+	</div>
+</body>
+</html>
